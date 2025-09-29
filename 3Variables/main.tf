9ce7a516ae0cec24acae6a5b72ed0d56089fe9b0
Vaidic Tiwari <72921704+Vaidic-afk@users.noreply.github.com>
2025-09-30 04:08:06 +0530

    Add files via upload

---
+++ 3Variables/main.tf
@@ -0,0 +1,62 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.41.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "backend-state-rg"
+      storage_account_name = "backend30681"
+      container_name       = "tfstate"
+      key                  = "test.terraform.tfstate"
+  }
+  required_version = "~>1.13.0"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+variable "environment" {
+    type    = string
+    default = "dev"
+    description = "Environment type"  
+}
+
+locals {
+  common_tags = {
+    env = "ppe"
+    owner = "vaidic"
+    act = "GRS"
+  }
+}
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "hello-world-rg"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_storage_account" "example" {
+    # depends_on = [ azurerm_resource_group.example ] # explicit dependency
+  name                = "vaidicsa14new"
+  resource_group_name = azurerm_resource_group.example.name # implicit dependency
+  location            = azurerm_resource_group.example.location
+  account_tier        = "Standard"
+  account_replication_type = local.common_tags.act
+
+  tags ={
+    environment = local.common_tags.env
+    owner = local.common_tags.owner
+  }
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+  description = "The name of the Storage Account"
+}
